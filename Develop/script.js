843959348288f00ff1590c1b875e869949f8fefd
Cnidarian <cmphillips23@gmail.com>
2020-12-21 00:01:30 -0600

    added prompt to include special characters; added feedback after each prompt to confirm the user's input; updated readme file

--- Develop/script.js
+++ Develop/script.js
@@ -1,39 +1,58 @@
 // Assignment code here
+var password = ""
+
 function generatePassword() {
-  var passwordArray = [""];
-  passwordLength = prompt("How many characters would you like your new password to have?  Enter a number between 8 and 128", "8 - 128");
-  if (passwordLength < 8 || passwordLength > 128) {
-    alert("Please enter a number from 8 to 128");
+  var passwordArray = [""]
+  if (passwordLength = prompt("How many characters would you like your new password to have?  Enter a number from 8 to 128.", "8 - 128")) {
+    if (passwordLength >= 8 && passwordLength <= 128) {
+      alert("Your password will be " + passwordLength + " characters in length.")
+    } else {
+      alert("Please enter a number from 8 to 128.")
+    }
+    if (confirm("Would you like your password to include lowercase letters?")) {
+      passwordArray.push("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
+      alert("Your password will include lowercase letters.")
+    } else {
+      alert("Your password will not include lowercase letters.")
+    }
+    if (confirm("Would you like your password to include uppercase letters?")) {
+      passwordArray.push("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
+      alert("Your password will include uppercase letters.")
+    } else {
+      alert("Your password will not include uppercase letters.")
+    }
+    if (confirm("Would you like your password to include numbers?")) {
+      passwordArray.push("0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
+      alert("Your password will include numbers.")
+    } else {
+      alert("Your password will not include numbers.")
+    }
+    if (confirm("Would you like your password to include special characters?")) {
+      passwordArray.push("!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", ">", "=", "?", "@", "[", "]", "^", "_", "`", "{", "}", "|", "~", String.fromCharCode(34), String.fromCharCode(92))
+      alert("Your password will include special characters.")
+    } else {
+      alert("Your password will not include special characters.")
+    }
+    for (var i=0; i < passwordLength; i++) {
+    passwordRandom = passwordArray[Math.floor(Math.random()*passwordArray.length)]
+    password = passwordRandom + password
+    }
   } else {
-    alert("Your password will be " + passwordLength + " characters in length");
+    alert("Please enter a length for your password.  Click the Generate Password button to try again!")
   }
-  if (confirm("Would you like your password to include lowercase letters?")) {
-    passwordArray.push("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
-  }
-  if (confirm("Would you like your password to include uppercase letters?")) {
-    passwordArray.push("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
-  }
-  if (confirm("Would you like your password to include numbers?")) {
-    passwordArray.push("0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
-  }
-  for (var i=0; i < passwordLength; i++) {
-  passwordRandom = passwordArray[Math.floor(Math.random()*passwordArray.length)]
-  password = passwordRandom + password
-  }
-  console.log(password)
 }
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate")
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  generatePassword()
+  var passwordText = document.querySelector("#password")
 
-  passwordText.value = password;
+  passwordText.value = password
 
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword)
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # password-generator
+
+This page generates a password based off of the criteria selected in the prompts.  Screenshot below shows end product after all prompts are answered and password is generated:
+
+![Password Generator](./develop/password-generator.png)
+
+Link to site - https://cmphillips23.github.io/password-generator/
